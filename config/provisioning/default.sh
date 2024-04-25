a2c3ebec8da3a139e3e14f8d6803071a3af8fb29
projetosTherion <159710027+projetosTherion@users.noreply.github.com>
2024-04-25 18:31:08 -0300

    Update default.sh

--- config/provisioning/default.sh
+++ config/provisioning/default.sh
@@ -21,11 +21,13 @@ CHECKPOINT_MODELS=(
 )
 
 LORA_MODELS=(
-    
+    #"https://civitai.com/api/download/models/16576"
 )
 
 VAE_MODELS=(
-   
+   "https://huggingface.co/stabilityai/sd-vae-ft-ema-original/resolve/main/vae-ft-ema-560000-ema-pruned.safetensors"
+    "https://huggingface.co/stabilityai/sd-vae-ft-mse-original/resolve/main/vae-ft-mse-840000-ema-pruned.safetensors"
+    "https://huggingface.co/stabilityai/sdxl-vae/resolve/main/sdxl_vae.safetensors"
 )
 
 ESRGAN_MODELS=(
