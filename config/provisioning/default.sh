af605cc352f6bf303cc97ca20bc02aec6e95f499
projetosTherion <159710027+projetosTherion@users.noreply.github.com>
2024-05-01 20:56:40 -0300

    Update default.sh

--- config/provisioning/default.sh
+++ config/provisioning/default.sh
@@ -1,109 +1,150 @@
 #!/bin/bash
 
-# Dependências: wget, git, conda
+# This file will be sourced in init.sh
+
+# https://raw.githubusercontent.com/ai-dock/comfyui/main/config/provisioning/default.sh
+
+# Packages are installed after nodes so we can fix them...
 
 PYTHON_PACKAGES=(
-    # "opencv-python==4.7.0.72"  # Exemplo de pacote Python, descomente e ajuste conforme necessário
+    #"opencv-python==4.7.0.72"
 )
 
 NODES=(
-    "https://github.com/ltdrdata/ComfyUI-Manager",
-    "https://github.com/yolain/ComfyUI-Easy-Use",
-    "https://github.com/Fannovel16/comfyui_controlnet_aux",
-    "https://github.com/kijai/ComfyUI-Marigold",
-    "https://github.com/LykosAI/ComfyUI-Inference-Core-Nodes",
-    "https://github.com/cubiq/ComfyUI_IPAdapter_plus",
+    "https://github.com/ltdrdata/ComfyUI-Manager"
+    "https://github.com/yolain/ComfyUI-Easy-Use"
+    "https://github.com/Fannovel16/comfyui_controlnet_aux"
+    "https://github.com/kijai/ComfyUI-Marigold"
+    "https://github.com/LykosAI/ComfyUI-Inference-Core-Nodes"
+    "https://github.com/cubiq/ComfyUI_IPAdapter_plus"
     "https://github.com/Coyote-A/ultimate-upscale-for-automatic1111"
 )
 
 CHECKPOINT_MODELS=(
-    "https://huggingface.co/runwayml/stable-diffusion-v1-5/resolve/main/v1-5-pruned-emaonly.ckpt",
+    "https://huggingface.co/runwayml/stable-diffusion-v1-5/resolve/main/v1-5-pruned-emaonly.ckpt"
     "https://drive.google.com/uc?id=1nUILIbv4Tqi6L6zqYYnFspKjD1qqdpOr"
+    
 )
 
 LORA_MODELS=(
-    # Exemplo: "https://drive.google.com/uc?id=YOUR_FILE_ID"
+    #"https://civitai.com/api/download/models/16576"
 )
 
 VAE_MODELS=(
-    "https://huggingface.co/stabilityai/sd-vae-ft-ema-original/resolve/main/vae-ft-ema-560000-ema-pruned.safetensors",
-    "https://huggingface.co/stabilityai/sd-vae-ft-mse-original/resolve/main/vae-ft-mse-840000-ema-pruned.safetensors",
+   "https://huggingface.co/stabilityai/sd-vae-ft-ema-original/resolve/main/vae-ft-ema-560000-ema-pruned.safetensors"
+    "https://huggingface.co/stabilityai/sd-vae-ft-mse-original/resolve/main/vae-ft-mse-840000-ema-pruned.safetensors"
     "https://huggingface.co/stabilityai/sdxl-vae/resolve/main/sdxl_vae.safetensors"
 )
 
 ESRGAN_MODELS=(
-    "https://huggingface.co/ai-forever/Real-ESRGAN/resolve/main/RealESRGAN_x4.pth",
-    "https://huggingface.co/FacehugmanIII/4x_foolhardy_Remacri/resolve/main/4x_foolhardy_Remacri.pth",
+    "https://huggingface.co/ai-forever/Real-ESRGAN/resolve/main/RealESRGAN_x4.pth"
+    "https://huggingface.co/FacehugmanIII/4x_foolhardy_Remacri/resolve/main/4x_foolhardy_Remacri.pth"
     "https://huggingface.co/Akumetsu971/SD_Anime_Futuristic_Armor/resolve/main/4x_NMKD-Siax_200k.pth"
+    "https://huggingface.co/lokCX/4x-Ultrasharp/blob/main/4x-UltraSharp.pth"
 )
 
 CONTROLNET_MODELS=(
-    "https://github.com/cubiq/ComfyUI_IPAdapter_plus",
-    "https://huggingface.co/lllyasviel/sd_control_collection/resolve/main/sai_xl_canny_256lora.safetensors?download=true",
-    "https://drive.google.com/uc?id=1QmgZFXkJoHNDiBVK8EqjmVeunbtDW9m6",
-    "https://huggingface.co/h94/IP-Adapter/resolve/main/sdxl_models/ip-adapter-plus_sdxl_vit-h.safetensors?download=true",
+    "https://github.com/cubiq/ComfyUI_IPAdapter_plus"
+    "https://huggingface.co/lllyasviel/sd_control_collection/resolve/main/sai_xl_canny_256lora.safetensors?download=true"
+    "https://drive.google.com/uc?id=1QmgZFXkJoHNDiBVK8EqjmVeunbtDW9m6"
+    "https://huggingface.co/h94/IP-Adapter/resolve/main/sdxl_models/ip-adapter-plus_sdxl_vit-h.safetensors?download=true"
     "https://huggingface.co/openai/clip-vit-large-patch14/resolve/main/model.safetensors?download=true"
 )
 
 ### DO NOT EDIT BELOW HERE UNLESS YOU KNOW WHAT YOU ARE DOING ###
 
 function provisioning_start() {
-    echo "Starting provisioning..."
+    DISK_GB_AVAILABLE=$(($(df --output=avail -m "${WORKSPACE}" | tail -n1) / 1000))
+    DISK_GB_USED=$(($(df --output=used -m "${WORKSPACE}" | tail -n1) / 1000))
+    DISK_GB_ALLOCATED=$(($DISK_GB_AVAILABLE + $DISK_GB_USED))
+    provisioning_print_header
     provisioning_get_nodes
     provisioning_install_python_packages
-    provisioning_get_models "ckpt" "${CHECKPOINT_MODELS[@]}"
-    provisioning_get_models "lora" "${LORA_MODELS[@]}"
-    provisioning_get_models "controlnet" "${CONTROLNET_MODELS[@]}"
-    provisioning_get_models "vae" "${VAE_MODELS[@]}"
-    provisioning_get_models "esrgan" "${ESRGAN_MODELS[@]}"
-    echo "Provisioning complete."
+    provisioning_get_models \
+        "${WORKSPACE}/storage/stable_diffusion/models/ckpt" \
+        "${CHECKPOINT_MODELS[@]}"
+    provisioning_get_models \
+        "${WORKSPACE}/storage/stable_diffusion/models/lora" \
+        "${LORA_MODELS[@]}"
+    provisioning_get_models \
+        "${WORKSPACE}/storage/stable_diffusion/models/controlnet" \
+        "${CONTROLNET_MODELS[@]}"
+    provisioning_get_models \
+        "${WORKSPACE}/storage/stable_diffusion/models/vae" \
+        "${VAE_MODELS[@]}"
+    provisioning_get_models \
+        "${WORKSPACE}/storage/stable_diffusion/models/esrgan" \
+        "${ESRGAN_MODELS[@]}"
+    provisioning_print_end
 }
 
 function provisioning_get_nodes() {
-    echo "Getting nodes..."
     for repo in "${NODES[@]}"; do
-        dir=$(basename "$repo")
+        dir="${repo##*/}"
         path="/opt/ComfyUI/custom_nodes/${dir}"
+        requirements="${path}/requirements.txt"
         if [[ -d $path ]]; then
-            echo "Updating node: ${dir}..."
-            (cd "$path" && git pull)
+            if [[ ${AUTO_UPDATE,,} != "false" ]]; then
+                printf "Updating node: %s...\n" "${repo}"
+                ( cd "$path" && git pull )
+                if [[ -e $requirements ]]; then
+                    micromamba -n comfyui run ${PIP_INSTALL} -r "$requirements"
+                fi
+            fi
         else
-            echo "Cloning node: ${dir}..."
-            git clone "${repo}" "${path}"
-        fi
-        if [[ -f "${path}/requirements.txt" ]]; then
-            echo "Installing requirements for ${dir}..."
-            conda install --yes --file "${path}/requirements.txt"
+            printf "Downloading node: %s...\n" "${repo}"
+            git clone "${repo}" "${path}" --recursive
+            if [[ -e $requirements ]]; then
+                micromamba -n comfyui run ${PIP_INSTALL} -r "${requirements}"
+            fi
         fi
     done
 }
 
 function provisioning_install_python_packages() {
-    if [[ ${#PYTHON_PACKAGES[@]} -gt 0 ]]; then
-        echo "Installing Python packages..."
-        conda install --yes "${PYTHON_PACKAGES[@]}"
+    if [ ${#PYTHON_PACKAGES[@]} -gt 0 ]; then
+        micromamba -n comfyui run ${PIP_INSTALL} ${PYTHON_PACKAGES[*]}
     fi
 }
 
 function provisioning_get_models() {
-    local category="$1"
-    shift
-    local models=("$@")
-    local dir="${WORKSPACE}/storage/stable_diffusion/models/${category}"
+    if [[ -z $2 ]]; then return 1; fi
+    dir="$1"
     mkdir -p "$dir"
-    echo "Downloading models to ${dir}..."
-    for url in "${models[@]}"; do
-        if ! provisioning_download "${url}" "${dir}"; then
-            echo "Failed to download model from ${url}"
-        fi
+    shift
+    if [[ $DISK_GB_ALLOCATED -ge $DISK_GB_REQUIRED ]]; then
+        arr=("$@")
+    else
+        printf "WARNING: Low disk space allocation - Only the first model will be downloaded!\n"
+        arr=("$1")
+    fi
+    
+    printf "Downloading %s model(s) to %s...\n" "${#arr[@]}" "$dir"
+    for url in "${arr[@]}"; do
+        printf "Downloading: %s\n" "${url}"
+        provisioning_download "${url}" "${dir}"
+        printf "\n"
     done
 }
 
+function provisioning_print_header() {
+    printf "\n##############################################\n#                                            #\n#          Provisioning container            #\n#                                            #\n#         This will take some time           #\n#                                            #\n# Your container will be ready on completion #\n#                                            #\n##############################################\n\n"
+    if [[ $DISK_GB_ALLOCATED -lt $DISK_GB_REQUIRED ]]; then
+        printf "WARNING: Your allocated disk size (%sGB) is below the recommended %sGB - Some models will not be downloaded\n" "$DISK_GB_ALLOCATED" "$DISK_GB_REQUIRED"
+    fi
+}
+
+function provisioning_print_end() {
+    printf "\nProvisioning complete:  Web UI will start now\n\n"
+}
+
+# Download from $1 URL to $2 file path
 function provisioning_download() {
-    local url="$1"
-    local dir="$2"
-    wget -q --show-progress --continue --directory-prefix="$dir" "$url"
-    return $?
+    wget -qnc --content-disposition --show-progress -e dotbytes="${3:-4M}" -P "$2" "$1"
+    wget --no-check-certificate 'https://drive.google.com/uc?id=1nUILIbv4Tqi6L6zqYYnFspKjD1qqdpOr' -O checkmodel
+    wget --no-check-certificate 'https://drive.google.com/uc?id=1QmgZFXkJoHNDiBVK8EqjmVeunbtDW9m6' -O upscaleadd
+
+
 }
 
 provisioning_start
