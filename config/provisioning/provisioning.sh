68b5128866666fa839d6587af46ce94c4179fe5a
arcseedai <arcseedai@gmail.com>
2025-02-27 16:03:59 -0300

    Update provisioning.sh

--- config/provisioning/provisioning.sh
+++ config/provisioning/provisioning.sh
@@ -15,7 +15,7 @@ PIP_PACKAGES=(
 PYTHON_PACKAGES=(
     "diffusers==0.28.0"
     #"transformers==4.32.0"
-    "huggingface_hub"
+    "huggingface-hub==0.25.2"
     # "opencv-python==4.7.0.72"
     "matplotlib"
     
